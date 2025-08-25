eab0880e1c97cd2ea581dc7f27cc936a8533cb02
Joshua Lucas <joshuamlucas91@gmail.com>
2025-08-25 08:29:58 -0400

    add diagnostic settings for default.

--- env/.config/nvim/lua/plugins/lsp-config.lua
+++ env/.config/nvim/lua/plugins/lsp-config.lua
@@ -48,6 +48,15 @@ return {
         capabilities = capabilities,
       })
 
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
